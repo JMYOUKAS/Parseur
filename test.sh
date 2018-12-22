1778c5b27b12ed204b745b8a4717a7749ab79e6f
charly <oursgris84@gmail.com>
2018-12-22 10:25:00 +0100

    PAR-008 : Dégager les informations nécessaires, récupération du nom de fichier en vue de l'écrire dans le fichier destination du parse

--- test.sh
+++ test.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
 # $1 : 1er argument
+if [ -d "$1/CONVERT" ]; then
+    rm -R "$1/CONVERT"
+fi
+if [ -d "$1/PARSE" ]; then
+    rm -R "$1/PARSE"
+fi
 
 mkdir $1/CONVERT
 mkdir $1/PARSE
 
-for f in $1/*.pdf; do
-    echo "$f"
+for f in $1/*.pdf
+do
+    nom=$(basename "$f" .pdf)
+    chemin="$1/CONVERT/$nom.txt"
+    pdftotext "$f" "$chemin"    
+done
+
+for f in $1/CONVERT/*.txt
+do
+    nouveau_nom=`echo $f | tr " " "_"`
+    if [ "$nouveau_nom" != "$f" ]; then
+        mv -i "$f" $nouveau_nom
+    fi
+done
+
+# Récupération du nom d'orgine
+for f in $1/*.pdf
+do
+    filename=$(basename "$f")
+    echo "$filename"
 done
