4772c01f3743ff6a4d0436af4a6c9367c7ba5437
uapv-lluch <guillaume.lluch@alumni.univ-avignon.fr>
2018-12-23 16:16:02 +0100

    PAR-10 : Parse (dans une seule boucle)

--- test.sh
+++ test.sh
@@ -23,8 +23,64 @@ do
     convertedTxtPath="$1/CONVERT/$name.txt"
     pdftotext "$pdfPath" "$convertedTxtPath"
 
-    echo "$name" >> "$1/PARSE/$name.txt"
+    echo "Nom du fichier : $name.pdf" >> "$1/PARSE/$name.txt"
 
+    f="$convertedTxtPath"
+
+    # TITRE
+    a=`expr $a + 1`
+    # echo ''
+    # echo "Nouveau document"
+    titre=''
+    cat "$f" | while read line; 
+    do
+        if [[ "$line" =~ [0-9] ]] || [[ "$line" =~ 'www' ]] || [[ "$line" =~ '@' ]]; then
+            if test -z "$titre"; then
+            continue
+            else
+                #echo "VOICI LE TITRE COMPLET"
+                #echo "$titre"
+                echo "Titre : $titre" >> "$1/PARSE/$name.txt"
+                # echo "i2 : $i"
+                break
+            fi
+        else
+            #Si le titre est vide
+            if test -z "$titre"; then
+                #echo "Debut Titre"
+                titre="$line"
+                #echo "$titre";
+            #Sinon
+            else
+                #si la ligne est vide
+                if test -z "$line"; then
+                    continue
+                #Sinon
+                else
+                    word=`echo "$line" | wc -w`
+                    #Si le nombre de mot est égale a 2               
+                    if test $word -eq 2; then
+                        echo "VOICI L E TITRE COMPLET"
+                        echo "$titre"
+                        echo "Titre : $titre" >> "$1/PARSE/$name.txt"
+                        echo "a = $a"
+                        break
+                    #Sinon               
+                    else 
+                        #RAJOUTER LA VERIFICATION DES VIRGULES TOUTS LES DEUX MOTS (Juste sur le premier c'est suffisant)
+                        #echo "Le titre"
+                        #echo "$titre"
+                        #echo "$line"
+                        titre="$titre $line"
+                        # echo "$titre"
+                    fi
+                fi
+            fi
+        fi
+    done
+
+
+    # RESUME / ABSTRACT
     #Affiche le numero de la ligne de l'Abstract
     # echo "Debut de l'abstract"
     debut=`cat "$convertedTxtPath" | (grep -n '[aA][bB][sS][tT][rR][aA][cC][tT]' | head -1) | cut -d: -f1`
@@ -41,5 +97,5 @@ do
     abstract=`cat "$convertedTxtPath" | sed -n $debut,$finSansIntroduction'p'`
     # echo $abstract
 
-    echo "$abstract" >> "$1/PARSE/$name.txt"
+    echo "Résumé : $abstract" >> "$1/PARSE/$name.txt"
 done
